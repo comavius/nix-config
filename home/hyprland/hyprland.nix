a7d651db56675922204509245a82c4d91bcdde03
comavius <comavius@proton.me>
2025-06-05 22:28:54 +0900

    ✨ conf file placing

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -1,13 +1,23 @@
-{...}: {
+{conf, ...}:
+let
+  hyprlandConfFiles = [
+    "keybindings.conf"
+  ];
+  hyprlandConfDirFromHomeDir = ".config/hypr";
+  hyprlandConfDir = "${conf.homeDirectory}/${hyprlandConfDirFromHomeDir}";
+in
+ {
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      source = [
-        # "./exec.conf"
-        "./keybindings.conf"
-      ];
+      source = builtins.map(confFile: "${hyprlandConfDir}/${confFile}") hyprlandConfFiles;
     };
   };
+  home.file."${hyprlandConfDirFromHomeDir}" = builtins.listToAttrs builtins.map (confFile: {
+    name = confFile;
+    value = { text = builtins.readFile "./${confFile}"; };
+  }) hyprlandConfFiles;
+
   programs.waybar = {
     enable = true;
     systemd.enable = true;
