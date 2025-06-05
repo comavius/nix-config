a2afb25df8a15f735c878934ac681cd2e320447f
comavius <comavius@proton.me>
2025-06-05 21:24:05 +0900

    ✨ hyprland and kitty

---
+++ home/hyprland/hyprland.nix
@@ -0,0 +1,26 @@
+{...}: {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      source = [
+        "./exec.conf"
+      ];
+    };
+  };
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+  };
+
+  # screen locker
+  programs.hyprlock.enable = true;
+
+  # Logout Menu
+  programs.wlogout.enable = true;
+
+  # Hyprland idle daemon
+  services.hypridle.enable = true;
+
+  # notification daemon, the same as dunst
+  services.mako.enable = true;
+}
