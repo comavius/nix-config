4216edc632b59108c1ae446e30709f166c8d64f6
comavius <comavius@proton.me>
2025-06-06 22:57:17 +0900

    🚧

--- rust/background/src/main.rs
+++ rust/background/src/main.rs
@@ -111,7 +111,7 @@ impl Dispatch<zwlr_layer_surface_v1::ZwlrLayerSurfaceV1, ()> for State {
 
 fn main() {
     let connection = Connection::connect_to_env().expect("Failed to connect to Wayland server");
-    // let display = connection.display();
+    let _display = connection.display();
     let mut event_queue: EventQueue<State> = connection.new_event_queue();
     let qhandle = event_queue.handle();
     // let registry = display.get_registry(&qhandle, ());
