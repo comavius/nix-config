e732f37fe856f6525c786f47e59d47555b345ed4
comavius <comavius@proton.me>
2025-08-01 09:36:54 +0900

    trivial change for lsp-integrated readability

--- core/utilities/unfree-utilities.nix
+++ core/utilities/unfree-utilities.nix
@@ -1,5 +1,7 @@
-{unfree-pkgs, ...}: {
-  environment.systemPackages = with unfree-pkgs; [
+{unfree-pkgs, ...}: let
+  pkgs = unfree-pkgs;
+in {
+  environment.systemPackages = with pkgs; [
     vscode
     discord
     steam
