b5592fc404ace3dd6369ee3ed35d842a1b0e124c
comavius <comavius@proton.me>
2025-08-01 15:32:09 +0900

    add temporary waybar settings

--- home/waybar/waybar.nix
+++ home/waybar/waybar.nix
@@ -1,6 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   programs.waybar = {
     enable = true;
     systemd.enable = true;
+    style = ''
+      @import url("${./waybar.css}");
+    '';
+    settings = builtins.fromJSON (builtins.readFile ./waybar.json);
   };
 }
